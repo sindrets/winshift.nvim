08e314f3e5babc6eb3e823d2caa27bd2983a1204
Sindre T. Strøm <sindrets@gmail.com>
2021-11-12 15:39:18 +0100

    feat: Support 256-color cterm highlights. (closes #7)

--- lua/winshift/colors.lua
+++ lua/winshift/colors.lua
@@ -2,6 +2,24 @@ local config = require("winshift.config")
 local api = vim.api
 local M = {}
 
+--#region TYPES
+
+---@class HiSpec
+---@field fg string
+---@field bg string
+---@field ctermfg integer
+---@field ctermbg integer
+---@field gui string
+---@field sp string
+---@field blend integer
+---@field default boolean
+
+---@class HiLinkSpec
+---@field force boolean
+---@field default boolean
+
+--#endregion
+
 ---@param name string Syntax group name.
 ---@param attr string Attribute name.
 ---@param trans boolean Translate the syntax group (follows links).
@@ -22,44 +40,113 @@ function M.get_hl_attr(name, attr, trans)
   return value
 end
 
----@param group_name string Syntax group name.
+---@param groups string|string[] Syntax group name, or an ordered list of
+---groups where the first found value will be returned.
 ---@param trans boolean Translate the syntax group (follows links). True by default.
-function M.get_fg(group_name, trans)
+function M.get_fg(groups, trans)
   if type(trans) ~= "boolean" then trans = true end
-  return M.get_hl_attr(group_name, "fg", trans)
+
+  if type(groups) == "table" then
+    local v
+    for _, group in ipairs(groups) do
+      v = M.get_hl_attr(group, "fg", trans)
+      if v then return v end
+    end
+    return
+  end
+
+  return M.get_hl_attr(groups, "fg", trans)
 end
 
----@param group_name string Syntax group name.
+---@param groups string|string[] Syntax group name, or an ordered list of
+---groups where the first found value will be returned.
 ---@param trans boolean Translate the syntax group (follows links). True by default.
-function M.get_bg(group_name, trans)
+function M.get_bg(groups, trans)
   if type(trans) ~= "boolean" then trans = true end
-  return M.get_hl_attr(group_name, "bg", trans)
+
+  if type(groups) == "table" then
+    local v
+    for _, group in ipairs(groups) do
+      v = M.get_hl_attr(group, "bg", trans)
+      if v then return v end
+    end
+    return
+  end
+
+  return M.get_hl_attr(groups, "bg", trans)
 end
 
----@param group_name string Syntax group name.
+---@param groups string|string[] Syntax group name, or an ordered list of
+---groups where the first found value will be returned.
 ---@param trans boolean Translate the syntax group (follows links). True by default.
-function M.get_gui(group_name, trans)
+function M.get_gui(groups, trans)
   if type(trans) ~= "boolean" then trans = true end
-  local hls = {}
-  local attributes = {
-    "bold",
-    "italic",
-    "reverse",
-    "standout",
-    "underline",
-    "undercurl",
-    "strikethrough"
-  }
+  if type(groups) ~= "table" then groups = { groups } end
 
-  for _, attr in ipairs(attributes) do
-    if M.get_hl_attr(group_name, attr, trans) == "1" then
-      table.insert(hls, attr)
+  local hls
+  for _, group in ipairs(groups) do
+    hls = {}
+    local attributes = {
+      "bold",
+      "italic",
+      "reverse",
+      "standout",
+      "underline",
+      "undercurl",
+      "strikethrough"
+    }
+
+    for _, attr in ipairs(attributes) do
+      if M.get_hl_attr(group, attr, trans) == "1" then
+        table.insert(hls, attr)
+      end
+    end
+
+    if #hls > 0 then
+      return table.concat(hls, ",")
+    end
+  end
+end
+
+---@param group string Syntax group name.
+---@param opt HiSpec
+function M.hi(group, opt)
+  local use_tc = vim.o.termguicolors
+  local g = use_tc and "gui" or "cterm"
+
+  if not use_tc then
+    if opt.ctermfg then
+      opt.fg = opt.ctermfg
+    end
+    if opt.ctermbg then
+      opt.bg = opt.ctermbg
     end
   end
 
-  if #hls > 0 then
-    return table.concat(hls, ",")
-  end
+  vim.cmd(string.format(
+    "hi %s %s %s %s %s %s %s",
+    opt.default and "def" or "",
+    group,
+    opt.fg and (g .. "fg=" .. opt.fg) or "",
+    opt.bg and (g .. "bg=" .. opt.bg) or "",
+    opt.gui and ((use_tc and "gui=" or "cterm=") .. opt.gui) or "",
+    opt.sp and ("guisp=" .. opt.sp) or "",
+    opt.blend and ("blend=" .. opt.blend) or ""
+  ))
+end
+
+---@param from string Syntax group name.
+---@param to? string Syntax group name. (default: `"NONE"`)
+---@param opt? HiLinkSpec
+function M.hi_link(from, to, opt)
+  opt = opt or {}
+  vim.cmd(string.format(
+    "hi%s %s link %s %s",
+    opt.force and "!" or "",
+    opt.default and "default" or "",
+    from,
+    to or "NONE"
+  ))
 end
 
 function M.get_colors()
@@ -77,16 +164,21 @@ function M.get_colors()
 end
 
 function M.get_hl_groups()
-  local focused_bg = M.get_bg(config.get_config().focused_hl_group) or "Visual"
+  local hl_focused = config.get_config().focused_hl_group
+  local reverse = M.get_hl_attr(hl_focused, "reverse") == "1"
+  local bg_focused = reverse
+    and (M.get_fg({ hl_focused, "Normal" }) or "white")
+    or (M.get_bg({ hl_focused, "Normal" }) or "white")
+  local fg_focused = reverse and (M.get_bg({ hl_focused, "Normal" }) or "black") or nil
 
   return {
-    Normal = { bg = focused_bg },
-    EndOfBuffer = { fg = focused_bg, bg = focused_bg },
-    LineNr = { fg = M.get_fg("LineNr"), bg = focused_bg, gui = M.get_gui("LineNr") },
-    CursorLineNr = { fg = M.get_fg("CursorLineNr"), bg = focused_bg, gui = M.get_gui("CursorLineNr") },
-    SignColumn = { fg = M.get_fg("SignColumn"), bg = focused_bg },
-    FoldColumn = { fg = M.get_fg("FoldColumn"), bg = focused_bg },
-    WindowPicker = { fg = "#ededed", bg = "#4493c8", gui = "bold" },
+    Normal = { fg = fg_focused, bg = bg_focused },
+    EndOfBuffer = { fg = bg_focused, bg = bg_focused },
+    LineNr = { fg = M.get_fg("LineNr"), bg = bg_focused, gui = M.get_gui("LineNr") },
+    CursorLineNr = { fg = M.get_fg("CursorLineNr"), bg = bg_focused, gui = M.get_gui("CursorLineNr") },
+    SignColumn = { fg = M.get_fg("SignColumn"), bg = bg_focused },
+    FoldColumn = { fg = M.get_fg("FoldColumn"), bg = bg_focused },
+    WindowPicker = { fg = "#ededed", bg = "#4493c8", ctermfg = 255, ctermbg = 33, gui = "bold" },
   }
 end
 
@@ -96,20 +188,12 @@ M.hl_links = {
 }
 
 function M.setup()
-  for name, v in pairs(M.get_hl_groups()) do
-    vim.cmd(
-      string.format(
-        "hi WinShift%s %s %s %s",
-        name,
-        v.fg and "guifg=" .. v.fg or "",
-        v.bg and "guibg=" .. v.bg or "",
-        v.gui and "gui=" .. v.gui or ""
-      )
-    )
+  for name, opt in pairs(M.get_hl_groups()) do
+    M.hi("WinShift" .. name, opt)
   end
 
   for from, to in pairs(M.hl_links) do
-    vim.cmd("hi def link WinShift" .. from .. " " .. to)
+    M.hi_link("WinShift" .. from, to, { default = true })
   end
 end
 
