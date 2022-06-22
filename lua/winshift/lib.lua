2a8e50d02fc449e1694ec1a75690cf4ab3262b12
Sindre T. Strøm <sindrets@gmail.com>
2022-06-22 14:47:19 +0200

    feat!: Make the window picker function configurable. (#11)
    BREAKING CHANGE:
    
    The configuration for the window picker has changed as a result of the
    function now being fully configurable. If you have previously configured
    the window picker, see `:h winshift.changelog-11` for more information
    about how to update your config.

--- lua/winshift/lib.lua
+++ lua/winshift/lib.lua
@@ -277,47 +277,66 @@ function M.next_node_vertical(leaf, dir)
   end
 end
 
+---@class WindowPickerFilterRules
+---@field cur_win boolean
+---@field floats boolean
+---@field filetype string[]
+---@field buftype string[]
+---@field bufname string[]
+
+---@class WindowPickerSpec
+---@field picker_chars string
+---@field filter_rules WindowPickerFilterRules
+---@field filter_func fun(winids: integer[]): integer[]
+
 ---Get user to pick a window. Selectable windows are all windows in the current
 ---tabpage.
----@param ignore table<integer, boolean>
+---@param opt? WindowPickerSpec
 ---@return integer|nil -- If a valid window was picked, return its id. If an
 ---       invalid window was picked / user canceled, return nil. If there are
 ---       no selectable windows, return -1.
-function M.pick_window(ignore)
+function M.pick_window(opt)
+  opt = opt or {}
   local tabpage = api.nvim_get_current_tabpage()
   local win_ids = api.nvim_tabpage_list_wins(tabpage)
-  local exclude = config.get_config().window_picker_ignore
+  local curwin = api.nvim_get_current_win()
+  local filter_rules = opt.filter_rules or {}
 
   local selectable = vim.tbl_filter(function (id)
-    local bufid = api.nvim_win_get_buf(id)
-    local bufname = api.nvim_buf_get_name(bufid)
-
-    if ignore[id] then
+    if filter_rules.cur_win and curwin == id then
+      return false
+    elseif filter_rules.floats and api.nvim_win_get_config(id).relative ~= "" then
       return false
     end
 
+    local bufid = api.nvim_win_get_buf(id)
+    local bufname = api.nvim_buf_get_name(bufid)
+
     for _, option in ipairs({ "filetype", "buftype" }) do
-      if vim.tbl_contains(exclude[option], vim.bo[bufid][option]) then
+      if vim.tbl_contains(filter_rules[option] or {}, vim.bo[bufid][option]) then
         return false
       end
     end
 
-    for _, pattern in ipairs(exclude.bufname) do
+    for _, pattern in ipairs(filter_rules.bufname or {}) do
       local regex = vim.regex(pattern)
       if regex:match_str(bufname) ~= nil then
         return false
       end
     end
 
-    local win_config = api.nvim_win_get_config(id)
-    return win_config.focusable and not win_config.external
+    return true
   end, win_ids)
+
+  if opt.filter_func then
+    selectable = opt.filter_func(selectable)
+  end
 
   -- If there are no selectable windows: return. If there's only 1, return it without picking.
   if #selectable == 0 then return -1 end
   if #selectable == 1 then return selectable[1] end
 
-  local chars = config.get_config().window_picker_chars:upper()
+  local chars = (opt.picker_chars or "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"):upper()
   local i = 1
   local win_opts = {}
   local win_map = {}
@@ -360,8 +379,8 @@ function M.pick_window(ignore)
 
   -- Restore window options
   for _, id in ipairs(selectable) do
-    for opt, value in pairs(win_opts[id]) do
-      api.nvim_win_set_option(id, opt, value)
+    for option, value in pairs(win_opts[id]) do
+      api.nvim_win_set_option(id, option, value)
     end
   end
 
@@ -568,7 +587,7 @@ function M.start_swap_mode()
   vim.cmd("redraw")
 
   local ok, err = pcall(function()
-    local target = M.pick_window({ [cur_win] = true })
+    local target = conf.window_picker()
 
     if target == -1 or target == nil then
       return
